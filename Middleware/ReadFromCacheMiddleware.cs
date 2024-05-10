1c25b476b085b2670234f9bb6dac3408e8e47ace
BradleyCSO <Osborne.Bradley1998@gmail.com>
2024-05-10 15:24:13 +0100

    WiP: Add middleware which uses Redis microservices across HTTP requests

---
+++ Middleware/ReadFromCacheMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+
+namespace PreferencesApi.Middleware;
+
+public class ReadFromCacheMiddleware(IHttpClientFactory httpClientFactory, RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        // Only handle GET requests
+        if (context.Request.Method != HttpMethods.Get)
+            return;
+
+        HttpRequestMessage? httpRequestMessage = new HttpRequestMessage(
+            HttpMethod.Get, $"http://localhost:5169{context.Request.Path.Value}");
+
+        HttpClient? httpClient = httpClientFactory.CreateClient();
+
+        HttpResponseMessage? response = await httpClient.SendAsync(httpRequestMessage);
+
+        if (response.StatusCode == HttpStatusCode.OK)
+            await context.Response.WriteAsJsonAsync(response.Content); // Write the response we got from the cache off the bat
+
+        else
+            await next(context); // Go to the next RateLimiting middleware in the pipeline
+    }
+}
---
+++ Middleware/WriteToCacheMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+namespace PreferencesApi.Middleware;
+
+public class WriteToCacheMiddleware(IHttpClientFactory httpClientFactory, RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        // Write to cache
+        HttpRequestMessage? httpRequestMessage = new HttpRequestMessage(
+            HttpMethod.Post, $"http://localhost:5169{context.Request.Path.Value}");
+
+        HttpClient? httpClient = httpClientFactory.CreateClient();
+
+        HttpResponseMessage? response = await httpClient.SendAsync(httpRequestMessage);
+
+        if (response.StatusCode == HttpStatusCode.BadRequest)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await context.Response.WriteAsync("Failed to write entry to cache.");
+        }
+        else
+            context.Response.StatusCode = StatusCodes.Status201Created;
+
+        await next(context);
+    }
+}
