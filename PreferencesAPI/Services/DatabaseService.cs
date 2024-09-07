88c653d9b4b11d3a4538172c4852eb5f1e1b74d1
BradleyCSO <Osborne.Bradley1998@gmail.com>
2024-09-07 13:51:41 +0100

    General tidy up, add README

--- PreferencesAPI/Services/DatabaseService.cs
+++ PreferencesAPI/Services/DatabaseService.cs
@@ -5,6 +5,15 @@ using RedisCacheWithRateLimitingWebAPI.MainAPI.Models.Responses;
 
 namespace RedisCacheWithRateLimitingWebAPI.MainAPI.Services;
 
+public interface IDatabaseService
+{
+    void CreatePreferencesTableIfNotExists();
+    Task<int?> CreatePreferenceAsync(CreatePreferenceRequest createPreferenceRequest);
+    Task<int?> UpdatePreferenceAsync(UpdatePreferenceRequest updatePreferenceRequest);
+    Task<List<GetPreferencesResponse>> GetPreferencesAsync(GetPreferencesRequest getPreferencesRequest);
+    Task<bool> DeletePreferenceAsync(DeletePreferenceRequest deletePreferenceRequest);
+}
+
 public class DatabaseService(NpgsqlConnection connection, ILogger<DatabaseService> logger) : IDatabaseService
 {
     public void CreatePreferencesTableIfNotExists()
